d44b94bc2446f4bfc07d58e82a6458fcd665c636
Glend Maatita <glend_maatita@yahoo.com>
2013-12-12 16:03:46 +0700

    support php 5.3

--- src/Socialism/LaravelOpauth/LaravelOpauthServiceProvider.php
+++ src/Socialism/LaravelOpauth/LaravelOpauthServiceProvider.php
@@ -39,14 +39,13 @@ class LaravelOpauthServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-        $this->app['opauth'] = $this->app->share(function ($app) {
-            $config = $app['config']->get("laravel-opauth::opauth");
+		$this->app['opauth'] = $this->app->share(function ($app) {
+			$config = $app['config']->get("laravel-opauth::opauth");
+			$config['security_salt'] = $app['config']->get('app.key');
+			$config['path'] = substr(route($app['config']->get("laravel-opauth::route"), array(null, null)), strlen(URL::to('/')));
 
-            $config['security_salt'] = $app['config']->get('app.key');
-            $config['path'] = substr(route($app['config']->get("laravel-opauth::route"), [null, null]), strlen(URL::to('/')));
-
-            return new Opauth($config, false);
-        });
+			return new Opauth($config, false);
+		});
 	}
 
 	/**
